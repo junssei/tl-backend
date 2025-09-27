db34055dffb5ba678317714e83dbf0eba9c17e19
juvncent <junevincentmagsayofernandez@gmail.com>
2025-09-27 17:49:13 +0800

    change port for node js

--- src/db.js
+++ src/db.js
@@ -1,11 +1,15 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set in environment variables");
+}
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.DATABASE_URL.includes("render.com")
     ? { rejectUnauthorized: false }
-    : false
+    : false,
 });
 
 module.exports = pool;
