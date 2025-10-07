84689b2769c3dc08b08b45e939d7be8804467a51
junssei <junevincentmagsayofernandez@gmail.com>
2025-10-08 05:29:13 +0800

    change port

--- src/server.js
+++ src/server.js
@@ -56,7 +56,7 @@ app.get("/users/:id/customers/profile/:id", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
