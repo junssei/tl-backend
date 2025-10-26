3098154bbd200c6fd2c23ff6ac5691e0971f4fd0
juvncent <junevincentmagsayofernandez@gmail.com>
2025-10-26 16:55:37 +0800

    modified customer profile path

--- src/server.js
+++ src/server.js
@@ -50,12 +50,13 @@ app.get('/users/:userid/customerlist', async (req, res) => {
 });
 
 // Customer Profile
-app.get('/user/:id/customer/:id/profile', async (req, res) => {
+app.get('/user/:userid/customer/:customerid/profile', async (req, res) => {
   try {
-    const { id } = req.params;
-    const result = await pool.query('SELECT * FROM customer WHERE id = $1', [
-      id,
-    ]);
+    const { customerid, userid } = req.params;
+    const result = await pool.query(
+      'SELECT * FROM customer WHERE id = $1 AND userid = $2',
+      [customerid, userid],
+    );
 
     if (result.rows.length === 0) {
       return res.status(404).json({ error: 'Customer not found' });
